98036af4dddfc458d58f793549ccbaa9090edb24
Hiep Le <>
2024-07-26 18:36:52 +0700

    [DR-002] init contracts

---
+++ contracts/MyContract.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    string public message;
+
+    constructor(string memory _message) {
+        message = _message;
+    }
+
+    function setMessage(string memory _message) public {
+        message = _message;
+    }
+}
